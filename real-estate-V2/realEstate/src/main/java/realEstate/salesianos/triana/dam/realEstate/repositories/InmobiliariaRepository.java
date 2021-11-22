17fbf1b1bbf5704816d97c2c0b1fc63537832a14
Delacruz-6 <gdlcruzguzman@gmail.com>
2021-11-22 10:29:10 +0100

    Agregado proyecto anterior

--- real-estate-V2/realEstate/src/main/java/realEstate/salesianos/triana/dam/realEstate/repositories/InmobiliariaRepository.java
+++
@@ -1,7 +0,0 @@
-package realEstate.salesianos.triana.dam.realEstate.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import realEstate.salesianos.triana.dam.realEstate.models.Inmobiliaria;
-
-public interface InmobiliariaRepository extends JpaRepository<Inmobiliaria, Long> {
-}

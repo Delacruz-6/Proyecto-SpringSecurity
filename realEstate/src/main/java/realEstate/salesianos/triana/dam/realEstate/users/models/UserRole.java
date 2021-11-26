15c5cba2b7b965ef97b9f0a069f74634e5bea089
Delacruz-6 <gdlcruzguzman@gmail.com>
2021-11-26 04:53:57 +0100

    added import sql

--- realEstate/src/main/java/realEstate/salesianos/triana/dam/realEstate/users/models/UserRole.java
+++ realEstate/src/main/java/realEstate/salesianos/triana/dam/realEstate/users/models/UserRole.java
@@ -2,6 +2,11 @@ package realEstate.salesianos.triana.dam.realEstate.users.models;
 
 public enum UserRole {
 
-    ADMIN, GESTOR, PROPIETARIO
+    ADMIN("GESTOR"), PROPIETARIO("GESTOR"), GESTOR("GESTOR");
 
+    private String valor;
+
+    private UserRole (String valor) {this.valor=valor;}
+    public String getValor(){return valor;}
+    public void setValor(String valor) { this.valor=valor;}
 }
